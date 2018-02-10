e2f9cc97d4882fdf96ab6944b483e0f71850b8d3
Arto Bendiken <arto@conreality.org>
2018-02-10 22:30:30 +0200

    Initialized and configured PostgreSQL.

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
+# See: https://www.postgresql.org/docs/10/static/creating-cluster.html
+# See: https://www.postgresql.org/docs/10/static/app-pg-ctl.html
+# See: https://www.postgresql.org/docs/10/static/app-initdb.html
+mkdir -p "$PGDATA"
+chown -R "$PGUSER:$PGUSER" "$PGDATA"
+chmod 700 "$PGDATA"
+su-exec "$PGUSER" pg_ctl -D "$PGDATA" -U "$PGUSER" initdb
+
+# See: https://www.postgresql.org/docs/10/static/auth-pg-hba-conf.html
+echo 'local all all trust' > "$PGDATA/pg_hba.conf"
+echo 'host all all all trust' >> "$PGDATA/pg_hba.conf"
+
+# See: https://www.postgresql.org/docs/10/static/server-start.html
+mkdir -p /run/postgresql
+chown -R "$PGUSER:$PGUSER" /run/postgresql
+chmod 2775 /run/postgresql
+
 exec "$@"
--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,11 @@ FROM alpine:3.7
 
 LABEL maintainer="Arto Bendiken <arto@conreality.org>"
 
-RUN apk add --no-cache erlang pllua postgresql
+RUN apk add --no-cache erlang pllua postgresql su-exec
 
-ENV LANG en_US.UTF-8
-
+ENV LANG="en_US.UTF-8"
+ENV PGUSER="postgres"
+ENV PGDATA="/srv/postgres"
 VOLUME /srv
 
 COPY .docker/entrypoint.sh .
