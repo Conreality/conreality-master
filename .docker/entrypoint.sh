83fff551ed9a5767e700a75c62a45a7b10ca356e
Arto Bendiken <arto@conreality.org>
2018-02-13 20:06:36 +0200

    Configured PostgreSQL to listen on the external interface.

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -6,11 +6,15 @@
 mkdir -p "$PGDATA"
 chown -R "$PGUSER:$PGUSER" "$PGDATA"
 chmod 700 "$PGDATA"
-su-exec "$PGUSER" pg_ctl -D "$PGDATA" -U "$PGUSER" initdb
+su-exec "$PGUSER" pg_ctl initdb -D "$PGDATA" -o "-U $PGUSER"
 
 # See: https://www.postgresql.org/docs/10/static/auth-pg-hba-conf.html
 echo 'local all all trust' > "$PGDATA/pg_hba.conf"
 echo 'host all all all trust' >> "$PGDATA/pg_hba.conf"
+
+# See: https://www.postgresql.org/docs/10/static/config-setting.html
+# See: https://www.postgresql.org/docs/10/static/runtime-config-connection.html
+echo "listen_addresses='*'" >> "$PGDATA/postgresql.conf"
 
 # See: https://www.postgresql.org/docs/10/static/server-start.html
 mkdir -p /run/postgresql
--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,12 @@ LABEL maintainer="Arto Bendiken <arto@conreality.org>"
 
 RUN apk add --no-cache erlang pllua postgresql su-exec
 
+ENV TZ="UTC"
 ENV LANG="en_US.UTF-8"
 ENV PGUSER="postgres"
 ENV PGDATA="/srv/postgres"
 VOLUME /srv
+EXPOSE 5432
 
 COPY .docker/entrypoint.sh .
 ENTRYPOINT ["./entrypoint.sh"]
