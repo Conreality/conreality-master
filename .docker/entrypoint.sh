cf195e936f57e785a5e00d6e2d686fe748b9e79e
Arto Bendiken <arto@conreality.org>
2018-02-26 23:05:59 +0200

    Removed unnecessary files now moved to conreality/docker.

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -8,7 +8,6 @@ fi
 mkdir -p /etc/dropbear
 chown root:root /etc/dropbear
 chmod 700 /etc/dropbear
-
 if [ ! -f /etc/dropbear/dropbear_ecdsa_host_key ]; then
   /usr/bin/dropbearkey -t ecdsa -f /etc/dropbear/dropbear_ecdsa_host_key
 fi
