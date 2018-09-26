6d6bd5779a2a17e40ff57d4d65165ef7c966c0f7
Arto Bendiken <arto@conreality.org>
2018-09-26 17:25:37 +0300

    Customized the Mix configuration.

--- config/config.exs
+++ config/config.exs
@@ -10,11 +10,11 @@ use Mix.Config
 
 # You can configure your application as:
 #
-#     config :conreality, key: :value
+#     config :conreality_master, key: :value
 #
 # and access this configuration in your application as:
 #
-#     Application.get_env(:conreality, :key)
+#     Application.get_env(:conreality_master, :key)
 #
 # You can also configure a 3rd-party app:
 #
--- mix.exs
+++ mix.exs
@@ -1,25 +1,50 @@
 defmodule Conreality.Master.MixProject do
   use Mix.Project
 
+  @name      "Conreality Master"
+  @version   File.read!("VERSION") |> String.trim
+  @github    "https://github.com/conreality/conreality-master"
+  @bitbucket "https://bitbucket.org/conreality/conreality-master"
+  @homepage  "https://conreality.org"
+
   def project do
     [
-      app: :conreality,
-      version: "0.1.0",
+      app: :conreality_master,
+      version: @version,
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      name: @name,
+      source_url: @github,
+      homepage_url: @homepage,
+      description: description(),
+      package: package(),
+      docs: [source_ref: @version, main: "readme", extras: []],
+      deps: deps(),
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {Conreality.Master.Application, []},
       extra_applications: [:logger],
-      mod: {Conreality.Master.Application, []}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp package do
+    [
+      files: ~w(lib priv src mix.exs CHANGES.rst README.rst UNLICENSE VERSION),
+      maintainers: ["Conreality.org"],
+      licenses: ["Public Domain"],
+      links: %{"GitHub" => @github, "Bitbucket" => @bitbucket},
+    ]
+  end
+
+  defp description do
+    """
+    Conreality master server.
+    """
+  end
+
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
