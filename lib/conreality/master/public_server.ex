080f2020467950102bc3182cfd566984b9e3cc69
Arto Bendiken <arto@conreality.org>
2019-01-09 14:08:50 +0200

    Updated the code for API changes.

--- lib/conreality/master/public_server.ex
+++ lib/conreality/master/public_server.ex
@@ -6,9 +6,9 @@ defmodule Conreality.Master.Public.Server do
     Conreality.RPC.AuthResponse.new(session_id: 0) # TODO
   end
 
-  @spec bye(Conreality.RPC.EmptyRequest.t(), GRPC.Server.Stream.t()) :: Conreality.RPC.EmptyResponse.t()
+  @spec bye(Conreality.RPC.Nothing.t(), GRPC.Server.Stream.t()) :: Conreality.RPC.Nothing.t()
   def bye(_request, _stream) do
-    Conreality.RPC.EmptyResponse.new()
+    Conreality.RPC.Nothing.new()
   end
 
   @spec hello(Conreality.RPC.HelloRequest.t(), GRPC.Server.Stream.t()) :: Conreality.RPC.HelloResponse.t()
@@ -16,8 +16,8 @@ defmodule Conreality.Master.Public.Server do
     Conreality.RPC.HelloResponse.new(version: "Elixir") # TODO
   end
 
-  @spec ping(Conreality.RPC.PingRequest.t(), GRPC.Server.Stream.t()) :: Conreality.RPC.PongResponse.t()
+  @spec ping(Conreality.RPC.Nothing.t(), GRPC.Server.Stream.t()) :: Conreality.RPC.Nothing.t()
   def ping(_request, _stream) do
-    Conreality.RPC.PongResponse.new()
+    Conreality.RPC.Nothing.new()
   end
 end
--- lib/conreality/master/session_server.ex
+++ lib/conreality/master/session_server.ex
@@ -1,23 +1,23 @@
 defmodule Conreality.Master.Session.Server do
   use GRPC.Server, service: Conreality.RPC.Session.Service
 
-  @spec ping(Conreality.RPC.PingRequest.t(), GRPC.Server.Stream.t()) :: Conreality.RPC.PongResponse.t()
+  @spec ping(Conreality.RPC.Nothing.t(), GRPC.Server.Stream.t()) :: Conreality.RPC.Nothing.t()
   def ping(_request, _stream) do
-    Conreality.RPC.PongResponse.new()
+    Conreality.RPC.Nothing.new()
   end
 
-  @spec send_event(Conreality.RPC.SendEventRequest.t(), GRPC.Server.Stream.t()) :: Conreality.RPC.IDResponse.t()
+  @spec send_event(Conreality.RPC.Event.t(), GRPC.Server.Stream.t()) :: Conreality.RPC.EventID.t()
   def send_event(_request, _stream) do
-    Conreality.RPC.IDResponse.new(id: 0) # TODO
+    Conreality.RPC.EventID.new(id: 0) # TODO
   end
 
-  @spec send_message(Conreality.RPC.SendMessageRequest.t(), GRPC.Server.Stream.t()) :: Conreality.RPC.IDResponse.t()
+  @spec send_message(Conreality.RPC.Message.t(), GRPC.Server.Stream.t()) :: Conreality.RPC.MessageID.t()
   def send_message(_request, _stream) do
-    Conreality.RPC.IDResponse.new(id: 0) # TODO
+    Conreality.RPC.MessageID.new(id: 0) # TODO
   end
 
-  @spec update_player(Conreality.RPC.UpdatePlayerRequest.t(), GRPC.Server.Stream.t()) :: Conreality.RPC.EmptyResponse.t()
+  @spec update_player(Conreality.RPC.PlayerStatus.t(), GRPC.Server.Stream.t()) :: Conreality.RPC.Nothing.t()
   def update_player(_request, _stream) do
-    Conreality.RPC.EmptyResponse.new() # TODO
+    Conreality.RPC.Nothing.new() # TODO
   end
 end
