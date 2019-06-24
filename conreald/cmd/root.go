97dc7cbc41189e0a4d6f962400b4b2898c3f4275
Arto Bendiken <arto@conreality.org>
2019-06-24 17:15:11 +0300

    Created the `conreald` executable.

---
+++ conreald/cmd/root.go
@@ -0,0 +1,61 @@
+/* This is free and unencumbered software released into the public domain. */
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var configFile string
+var debug bool
+var verbose bool
+
+// RootCmd describes the `conreald` command
+var RootCmd = &cobra.Command{
+	Use:     "conreald",
+	Short:   "Conreality Master",
+	Version: "0.0.0", // FIXME
+}
+
+// Execute implements the `conreald` command
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	RootCmd.PersistentFlags().StringVarP(&configFile, "config", "C", "", "Set config file (default: $HOME/.conreality/config.yaml)")
+	RootCmd.PersistentFlags().BoolVarP(&debug, "debug", "d", false, "Enable debugging")
+	RootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Be verbose")
+	RootCmd.SetVersionTemplate(`Conreality Master {{printf "%s" .Version}}
+`)
+}
+
+// initConfig reads the configuration file and environment variables.
+func initConfig() {
+	if configFile != "" {
+		// Use the specified config file:
+		viper.SetConfigFile(configFile)
+	} else {
+		// Search for config file in the current directory and under the home directory:
+		viper.SetConfigName("config")
+		viper.AddConfigPath(".")
+		viper.AddConfigPath("$HOME/.conreality")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in:
+	if err := viper.ReadInConfig(); err == nil {
+		if debug {
+			fmt.Println("Using config file:", viper.ConfigFileUsed())
+		}
+	}
+}
---
+++ conreald/main.go
@@ -0,0 +1,10 @@
+/* This is free and unencumbered software released into the public domain. */
+
+// conreald
+package main
+
+import "github.com/conreality/conreality-master/conreald/cmd"
+
+func main() {
+	cmd.Execute()
+}
